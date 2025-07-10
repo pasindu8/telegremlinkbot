801c077c3a7cde254a622a1ade47c34ef673a47a
Pasindu Dulshan <75705950+pasindu8@users.noreply.github.com>
2025-07-10 19:55:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR_API_TOKEN';
+
+const token = '8188319392:AAEQmB_Dhu-x4GQAay-4Ar3XCke4G78_GnY'; 
+
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
@@ -9,9 +11,16 @@ bot.on('message', (msg) => {
   if (document) {
     const fileId = document.file_id;
     bot.getFileLink(fileId).then((link) => {
-      bot.sendMessage(chatId, 'Direct download link: ${link}');
+     
+      bot.sendMessage(chatId, `Direct download link: ${link}`); 
+    }).catch((error) => {
+      console.error("Error getting file link:", error);
+      bot.sendMessage(chatId, "Sorry, I couldn't get the file link. Please try again later.");
     });
   } else {
     bot.sendMessage(chatId, 'Please send a document to get a direct download link.');
   }
 });
+
+
+console.log('Bot is running...');
