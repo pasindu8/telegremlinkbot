92467f1dc51220a9010dad5e7854268a6543b4bc
Pasindu Dulshan <75705950+pasindu8@users.noreply.github.com>
2025-07-10 20:20:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8188319392:AAEQmB_Dhu-x4GQAay-4Ar3XCke4G78_GnY'; 
 
-const bot = new TelegramBot(token, { polling: true });
+const TOKEN = '8188319392:AAEQmB_Dhu-x4GQAay-4Ar3XCke4G78_GnY'; 
+
+
+const bot = new TelegramBot(TOKEN);
+
+
+module.exports = async (req, res) => {
+  try {
+  
+    if (req.body) {
+      bot.processUpdate(req.body); 
+    }
+   
+    res.status(200).send('OK'); 
+  } catch (error) {
+    console.error('Error while processing update:', error);
+    res.status(500).send('Error');
+  }
+};
+
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -14,13 +32,12 @@ bot.on('message', (msg) => {
      
       bot.sendMessage(chatId, `Direct download link: ${link}`); 
     }).catch((error) => {
-      console.error("Error getting file link:", error);
+      console.error("Error while retrieving file link:", error);
       bot.sendMessage(chatId, "Sorry, I couldn't get the file link. Please try again later.");
     });
   } else {
     bot.sendMessage(chatId, 'Please send a document to get a direct download link.');
   }
 });
-
 
 console.log('Bot is running...');
